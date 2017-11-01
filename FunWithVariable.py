be3877159b1122496096d9a0ff344c7e35fadc72
303PC03 <tonjelona@gmail.com>
2017-11-01 20:17:43 +0100

    Example of using variables.

---
+++ FunWithVariable.py
@@ -0,0 +1,16 @@
+
+
+#this is a variable
+radius = 10
+
+#this is a constant
+myConstpi = 3.14
+
+print("The radius of the circle is", radius)
+
+
+print("The circumference of a circle is:", 2*myConstpi*radius)
+
+print("The circumference of a circle is:", int(2*myConstpi*radius)
+
+
