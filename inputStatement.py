784ff45ea1d669828cfd798784dfd483e12be1ee
303PC03 <tonjelona@gmail.com>
2017-11-01 20:46:55 +0100

    Introduced an if condition to check for negative numbers

--- inputStatement.py
+++ inputStatement.py
@@ -8,6 +8,10 @@ print("Please enter the radus of the circle :")
 radius = float(input())
 print("you have entered a radius of", radius)
 
+if(radius < 0):
+    print("The radius of a circle cannot be a negative number")
+
+
 myConstPi = 3.14
 
 print("the circumgerence of the circle is :", 2*myConstPi*radius)
